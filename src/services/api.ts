8ddb44f44fbc6e9572c249b18bd74804dd341272
Khalid Shaikh <143262176+shk-khalid@users.noreply.github.com>
2025-05-22 22:14:41 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { authService } from './authService';
 
-const API_URL = 'https://web-production-d53f.up.railway.app/';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
